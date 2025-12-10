2d23a4758c6c8c76b2ad88aba5dd9585f1337c1f
luccorrales <74154774+luccorrales@users.noreply.github.com>
2025-12-09 21:03:59 -0500

    Update percentile-cognitive.js

--- js/percentile/percentile-cognitive.js
+++ js/percentile/percentile-cognitive.js
@@ -1,11 +1,11 @@
 // percentile-cognitive.js
-export const cognitivePercentiles = {
-  // Add cognitive percentile functions here
-  // Example future function:
-  // iq_percentile(value) {
-  //   const mean = 100;
-  //   const sd = 15;
-  //   const z = (value - mean) / sd;
-  //   return Math.round(cumulativeNormalDistribution(z) * 100);
-  // },
+
+percentileFunctions.iq_percentile = (value) => {
+  const mean = 100;
+  const sd = 15;
+  const z = (value - mean) / sd;
+  // cumulativeNormalDistribution is globally available from index.js
+  return Math.round(cumulativeNormalDistribution(z) * 100);
 };
+
+// Add other cognitive percentile functions here
