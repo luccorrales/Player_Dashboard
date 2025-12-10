bc2e5eca157c476edd8c86be37bd9e896dc0a7f1
luccorrales <74154774+luccorrales@users.noreply.github.com>
2025-12-09 21:04:58 -0500

    Update percentile-physical.js

--- js/percentile/percentile-physical.js
+++ js/percentile/percentile-physical.js
@@ -8,13 +8,13 @@ percentileFunctions.bench_press_percentile(weight, age, bodyweight) => {
   return calculatePercentileFromBenchmarks(adjustedRatio, benchmarks);
 };
 
-percentileFunctions.strength_ratio_percentile(weight, bodyweight) {
+percentileFunctions.strength_ratio_percentile(weight, bodyweight) => {
   const ratio = weight / bodyweight;
   const benchmarks = [0, 0.3, 0.5, 0.75, 1.0, 1.25, 1.5, 2.0];
   return calculatePercentileFromBenchmarks(ratio, benchmarks);
 };
 
-percentileFunctions.run_5k_percentile(minutes, age, gender) {
+percentileFunctions.run_5k_percentile(minutes, age, gender) => {
   const isMale = gender === "male";
   const ageFactor =
     age < 30 ? 1 : age < 40 ? 0.95 : age < 50 ? 0.9 : 0.85;
